a82d257c1238ea99b1d70b0916ec9e4f6c6f6b6d
amirg <agholizadgha@mun.ca>
2025-06-06 00:46:49 -0230

    fix signed in

--- app/(protected)/layout.tsx
+++ app/(protected)/layout.tsx
@@ -1,16 +1,23 @@
-import { RedirectToSignIn, SignedIn, SignedOut } from "@clerk/nextjs"
+"use client"
+
+import { RedirectToSignIn, useAuth } from "@clerk/nextjs"
 
 interface RootLayoutProps {
   children: React.ReactNode
 }
 
 export default function RootLayout({ children }: RootLayoutProps) {
+  const { isSignedIn } = useAuth()
+
+  if (isSignedIn === undefined) return null // or loading spinner
+
+  if (!isSignedIn) {
+    return <RedirectToSignIn />
+  }
+
   return (
     <section className="flex min-h-screen flex-col items-center justify-center w-full">
-      <SignedIn>{children}</SignedIn>
-      <SignedOut>
-        <RedirectToSignIn />
-      </SignedOut>
+      {children}
     </section>
   )
 }
